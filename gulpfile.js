a4c41c0c94d5b73f8ea994daefce731815ca6f43
Alamito <alamirbobroski12@gmail.com>
2023-04-21 13:07:30 -0300

    add coments

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,20 @@ const { series } = require('gulp');
 const watch = require('gulp-watch');
 const ts = require('gulp-typescript');
 
+// Função para verificar alterações nos arquivos TS
 checkTS = (cb) => {
-    watch('src/**/*.ts', series(makeJS));
+    watch('src/**/*.ts', series(makeJS)); // Verifica alterações nos arquivos TS
     return cb();
 };
 
+// Função para criar o arquivo JS
 makeJS = () => {
     return gulp
-        .src('src/**/*.ts')
-        .pipe(ts())
-        .pipe(gulp.dest('./build'))
-        .on('end', () => console.log('Arquivo JS criado com sucesso!'))
-        .on('error', (err) => console.log(err));
+        .src('src/**/*.ts') // Local dos arquivos TS
+        .pipe(ts())     // Compilação
+        .pipe(gulp.dest('./build')) // Destino
+        .on('end', () => console.log('Arquivo JS criado com sucesso!')) // Mensagem de sucesso
+        .on('error', (err) => console.log(err)); // Mensagem de erro
 };
 
-module.exports.default = series(checkTS, makeJS);
+module.exports.default = series(checkTS, makeJS); // Executa as funções em série
